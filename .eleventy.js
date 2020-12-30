f8d15a6cb75c3a12cbd1dde88cd44194a2a748f1
Trevor Bennett <github@trevorbennett.us>
2020-12-30 01:22:54 -0500

    add favicon info

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy('css')
+    eleventyConfig.addPassthroughCopy('css');
+    eleventyConfig.addPassthroughCopy('resources');
+
     return {
       passthroughFileCopy: true
     }
